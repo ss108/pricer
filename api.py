b3c77dac73dbd09f5837c8a91757ae4603b85449
Samier <samier_saeed@brown.edu>
2015-09-18 11:21:49 -0700

    yay cors

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask.ext.cors import CORS
 from pc_pp import get_part_info, get_average_price
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
