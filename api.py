1946d5948881f87355795417d5abf1b2e6549ee9
Samier <samier_saeed@brown.edu>
2015-09-18 16:47:38 -0700

    q type poking

--- api.py
+++ api.py
@@ -6,6 +6,12 @@ from pc_pp import get_part_info, get_average_price
 app = Flask(__name__)
 CORS(app)
 
+def _get_q_type(json_payload):
+    try:
+        return json_payload["q_type"]
+    except KeyError:
+        return ""
+
 @app.route('/')
 def index():
     return "welcome"
@@ -14,7 +20,14 @@ def index():
 def price():
     try:
         # return request
-        code = request.get_json(force=True)["part_no"]
+        query_type = _get_q_type(request.get_json(force=True)) 
+
+        if query_type == "keyword":
+            #some stuff
+            pass
+        else:
+            code = request.get_json(force=True)["part_no"]
+
         return str(get_average_price(code))
     except Exception as ex:
         return "0"
