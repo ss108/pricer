ca8f7798efaf74510faee5c836b99e312f198566
Samier <samier_saeed@brown.edu>
2015-09-28 17:53:26 -0700

    keyword later

--- api.py
+++ api.py
@@ -6,11 +6,13 @@ from pc_pp import get_part_info, get_average_price
 app = Flask(__name__)
 CORS(app)
 
-def _get_q_type(json_payload):
-    try:
-        return json_payload["q_type"]
-    except KeyError:
-        return ""
+# TODO: support keyword lookup (or else this tool will suck mucho : ))))
+# def _get_q_type(json_payload):
+#     try:
+#         return json_payload["q_type"]
+#     except KeyError:
+#         return ""
+
 
 @app.route('/')
 def index():
@@ -19,14 +21,7 @@ def index():
 @app.route('/price', methods=['POST'])
 def price():
     try:
-        # return request
-        query_type = _get_q_type(request.get_json(force=True)) 
-
-        if query_type == "keyword":
-            #some stuff
-            pass
-        else:
-            code = request.get_json(force=True)["part_no"]
+        code = request.get_json(force=True)["part_no"]
 
         return str(get_average_price(code))
     except Exception as ex:
