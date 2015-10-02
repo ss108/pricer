75a3eb88211ee479ac729dc2ae493257cf4e0071
Samier Saeed <samier_saeed@brown.edu>
2015-10-01 22:47:59 -0700

    fixed unit test(s)

--- tests/test_numerical.py
+++ tests/test_numerical.py
@@ -1,10 +1,10 @@
 import pytest
 from utils import to_number
 
-@pytest.mark.parametrize("string_repr","expected",[
+@pytest.mark.parametrize("string,expected",[
     ("$56", 56.0),
     ("$6", 6.0),
     ("$76.45", 76.45)
 ])
-def test_to_number(string_repr, expected):
-    assert to_number(string_repr) == expected
+def test_to_number(string, expected):
+    assert to_number(string) == expected
