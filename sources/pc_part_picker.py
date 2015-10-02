a31dc87c1c23f49c18247d5e66332e5f7f1ce5a0
Samier Saeed <samier_saeed@brown.edu>
2015-10-01 22:14:08 -0700

    forgot self for a second lulz

--- sources/pc_part_picker.py
+++ sources/pc_part_picker.py
@@ -13,27 +13,38 @@ class PCPartPicker(Source):
         self._part_path = "part/{slug}"
         self._search_path = "search/?live=1&qid=1&q={query}"
 
-    def _get_part_info(part_no):
+    def _get_part_info(self, part_no):
         result = requests.get(self._web_root + self._search_path.format(query = part_no))
         result_json = result.json()
         return {"slug": result_json["results"][0]["slug"]}
 
-    def _get_page_for_product(slug):
-        url = WEB_ROOT + PART_PATH.format(slug = slug)
+    def _get_page_for_product(self, slug):
+        url = self._web_root + self._part_path.format(slug = slug)
         page_response = requests.get(url)
         page_text = page_response.text
         page = BeautifulSoup(page_text)
         return page
 
-    def _extract_prices_from_page(product_page):
+    def _extract_prices_from_page(self, product_page):
         total_table_data = product_page.find_all(attrs = {'class': 'total'}) #TODO: use "base"
         prices = [_extract_total(td) for td in total_table_data]
         return filter(lambda p: p != 0, prices) #removes cruft that's not actually a price, but empty html element. Need to make sure not to fail to consider 0 val prices in future.
 
-    def _extract_total(total_el):
+    def _extract_total(self, total_el):
         try:
             p_link = total_el.contents[0]
             total_text = p_link.contents[0]
             return to_number(total_text)
         except:
             return 0
+
+    def get_all_prices(self, part_no):
+        part_info = self._get_part_info(part_no)
+        page = self._get_page_for_product(part_info["slug"])
+        return self._extract_prices_from_page(page)
+
+    def calc_average_price(self, part_no):
+        prices = self.get_all_prices(part_no)
+        return (sum(prices)/len(prices))
+
+
--- sources/source.py
+++ sources/source.py
@@ -4,10 +4,10 @@ class Source(object):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def get_prices(item_code):
+    def get_all_prices(self, item_code):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def average_price(item_code):
+    def calc_average_price(self, item_code):
         raise NotImplementedError()
 
